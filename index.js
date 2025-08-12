2bbb5e8bd3c132dd007b2de1527f3c90efd944cc
uday07566 <udaygogineni07@gmail.com>
2025-08-12 09:19:12 +0000

    Modifed Verision

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const app = express();
 const port=3000;
+var data;
 const _dirname=dirname(fileURLToPath(import.meta.url));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,7 +12,26 @@ app.get("/",(req,res)=>{
     res.sendFile(_dirname+"/public/index.html");
 });
 app.post("/submit",(req,res)=>{
-    res.render("index.ejs");
+    switch(req.body.operator){
+        case '+':
+          data=parseInt(req.body.num1)+parseInt(req.body.num2);
+          break;
+        case '-':
+          data=parseInt(req.body.num1)-parseInt(req.body.num2);
+          break;
+        case '*':
+          data=parseInt(req.body.num1)*parseInt(req.body.num2);
+          break;
+        case '/':
+          data=parseInt(req.body.num1)/parseInt(req.body.num2);
+          break;
+    }
+    res.render("index.ejs",{
+        value:data,
+        op:req.body.operator,
+        n1:req.body.num1,
+        n2:req.body.num2
+    });
 });
 app.listen(port,()=>{
     console.log("Server running is "+port)
